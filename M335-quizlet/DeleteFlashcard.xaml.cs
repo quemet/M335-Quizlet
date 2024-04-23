2f9431f272de3227620c7f14c9d69bfe156a144f
Quentin <quentinmet7@gmail.com>
2024-04-23 19:11:47 +0200

    Fin du devoir

---
+++ M335-quizlet/DeleteFlashcard.xaml.cs
@@ -0,0 +1,9 @@
+namespace M335_quizlet;
+
+public partial class DeleteFlashcard : ContentPage
+{
+	public DeleteFlashcard()
+	{
+		InitializeComponent();
+	}
+}
---
+++ M335-quizlet/ModifyFlashCard.xaml.cs
@@ -0,0 +1,9 @@
+namespace M335_quizlet;
+
+public partial class ModifyFlashCard : ContentPage
+{
+	public ModifyFlashCard()
+	{
+		InitializeComponent();
+	}
+}
